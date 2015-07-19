63992a7f691837d3c994629636ffde3c147f815c
David <yoxisem544@gmail.com>
2015-07-19 17:54:18 +0800

    setup new view controller: course detail view

---
+++ ColorgyCourse/ColorgyCourseDetailPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ColorgyCourseDetailPageViewController.swift
+//  ColorgyCourse
+//
+//  Created by David on 2015/7/19.
+//  Copyright (c) 2015年 David. All rights reserved.
+//
+
+import UIKit
+
+class ColorgyCourseDetailPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
